516e7eb3c426bb0a1433c4363932c00b213ce2f6
Escohen <escohen98@gmail.com>
2018-10-13 15:25:44 -0700

    Added warning message. Added icon.

---
+++ warning.js
@@ -0,0 +1,5 @@
+(function() {
+  "use strict"
+  window.addEventListener("load", initialize);
+  alert("This website is still under construction. Please visit https://www.facebook.com/uwzbt/ to get in contact with our house. Thank you!");
+})();
