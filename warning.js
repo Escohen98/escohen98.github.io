7c4fd3e85200c976fefde5eb24a25d62fa0d88c9
Escohen <escohen98@gmail.com>
2018-10-15 13:58:53 -0700

    Added embedded alert message to index.html. Removed warning.js.

--- warning.js
+++
@@ -1,5 +0,0 @@
-(function() {
-  "use strict"
-  window.addEventListener("load", initialize);
-  alert("This website is still under construction. Please visit https://www.facebook.com/uwzbt/ to get in contact with our house. Thank you!");
-})();
